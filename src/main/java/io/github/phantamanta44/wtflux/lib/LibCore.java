785881bbd286f8e9ff6045c5a8b7f6091d8b9908
phantamanta44 <evandalong@gmail.com>
2017-07-09 13:53:27 -0500

    Add another digit to the version number

--- src/main/java/io/github/phantamanta44/wtflux/lib/LibCore.java
+++ src/main/java/io/github/phantamanta44/wtflux/lib/LibCore.java
@@ -3,7 +3,7 @@ package io.github.phantamanta44.wtflux.lib;
 public final class LibCore {
 
     public static final String MODID = "wtflux";
-    public static final String MODVER = "1.0";
+    public static final String MODVER = "1.0.0";
     public static final String MODPREF = MODID + ":";
     
     public static final int GUI_FONT_COLOR = 4210752;
